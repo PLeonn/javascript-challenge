1d2867ebd471b4ee8dfef2b64f7db92a1305e8e2
Pleon <patrickedwardleon@gmail.com>
2020-10-03 17:05:19 -0700

    Table code for app.js

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,20 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+// Reference table body
+var t_body = d3.select("tbody");
+
+// Append table row and cell body tags onto data table with a nested for loop
+// Append table rows here
+tableData.forEach((sighting) => {
+    let t_row = t_body.append("tr");
+
+// Append cell body tags here
+    Object.values(sighting).forEach(value => {
+        let t_cell = t_row.append("td");
+        t_cell.text(value);
+
+    });
+});
+
+// Create event handler functionality for the application
